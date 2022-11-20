a76a5fa4e098f6d676d2e256dce9a32e450db455
Tom <artzhubrikov@mail.ru>
2022-11-20 14:52:31 +0300

    Step_4_div

---
+++ Step_4_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿double numA = new Random().Next(0, 100);
+Console.WriteLine(numA);
+double numB = new Random().Next(1, 100);
+Console.WriteLine(numB);
+double res = numA / numB;
+Console.WriteLine(res);
